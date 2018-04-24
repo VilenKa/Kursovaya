185c291a3562791023048c682f81bc6644d5a450
VilenKa <vilevat@mail.ru>
2018-04-25 01:54:43 +0500

    save

--- app/src/main/java/com/example/vilev/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/vilev/myapplication/MainActivity.kt
@@ -5,38 +5,36 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.R.attr.port
-import android.os.AsyncTask
-import com.example.vilev.myapplication.MainActivity.Companion.serIpAddress
 import java.io.DataOutputStream
 import java.net.InetAddress
 import java.net.Socket
-import android.os.AsyncTask.execute
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import io.reactivex.disposables.Disposable
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
-import java.net.ServerSocket
-import android.system.Os.socket
 import kotlinx.android.synthetic.main.layout1.*
-import java.io.ByteArrayOutputStream
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
 import java.io.DataInputStream
-import java.io.OutputStream
-import android.R.array
-import kotlinx.coroutines.experimental.delay
-import java.lang.reflect.Array.getShort
 import java.nio.ByteBuffer
+import moe.codeest.rxsocketclient.meta.ThreadStrategy
+import kotlin.text.Charsets
+import moe.codeest.rxsocketclient.meta.SocketConfig
+import moe.codeest.rxsocketclient.RxSocketClient
+import moe.codeest.rxsocketclient.SocketClient
+import moe.codeest.rxsocketclient.SocketSubscriber
+import java.net.SocketOption
 
 
 var ipd = ""
 class MainActivity : AppCompatActivity() {
     companion object {
         var serIpAddress: String? = null       // адрес сервера
-        var port = 115        // порт
+        var port = 1089       // порт
         var msg: String = ""               // Сообщение
         val codeMsg: Byte = 1 // Оправить сообщение
-        val codeHello: Byte = 42
+        val codeHello: Byte = 3
         var codeCommand: Byte = 0
     }
 
@@ -62,33 +60,56 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun Heloo() = runBlocking{
-        launch { netShit(codeHello) }
-        launch{ listen() }
+    fun Heloo(){
 
-    }
+//        launch{ listen() }
+//                launch { netShit(codeHello) }
 
+        val mClient = RxSocketClient
+                .create(SocketConfig.Builder()
+                        .setIp(ipd)
+                        .setPort(port)
+                        .setCharset(Charsets.UTF_8)
+                        .setThreadStrategy(ThreadStrategy.ASYNC)
+                        .setTimeout(30 * 1000)
+                        .build())
 
+        var ref  = mClient.connect()
+        ref.subscribe(object : SocketSubscriber(){
+            override fun onConnected() {
+                mClient.sendData("3")
+            }
 
-    suspend fun listen() {
-        val inputStream: DataInputStream
-        val socket = Socket(ipAddress, port)
-        inputStream = DataInputStream(socket.getInputStream())
-        while (true){
-           if(inputStream.read() > -1) {
-               val theBytes = ByteArray(inputStream.available())
-               inputStream.read(theBytes, 0, inputStream.available())
-               inputStream.close()
-               if (theBytes.toInt() == 42) {
-                   Toast.makeText(applicationContext, "HI!", Toast.LENGTH_LONG)
-               }
-           }
+            override fun onDisconnected() {
+                print("h")
+            }
+
+            override fun onResponse(data: ByteArray) {
+                launch(UI) { Toast.makeText(applicationContext, "HI!", Toast.LENGTH_LONG).show() }
+            }
+
         }
-
-
-
+        )
 
     }
+
+
+
+//    suspend fun listen() {
+//        val inputStream: DataInputStream
+//        val socket = Socket(ipAddress, port)
+//        inputStream = DataInputStream(socket.getInputStream())
+//        while (true){
+//           if(inputStream.read() > -1) {
+//               val theBytes = ByteArray(inputStream.available())
+//               inputStream.read(theBytes, 0, inputStream.available())
+//               inputStream.close()
+//               if (theBytes.toInt() == 42) {
+//                   Toast.makeText(applicationContext, "HI!", Toast.LENGTH_LONG).show()
+//               }
+//           }
+//        }
+//    }
 
 
     fun onConnectClick() = runBlocking {
@@ -97,7 +118,7 @@ class MainActivity : AppCompatActivity() {
             ipd = etIPaddress.text.toString()
             // ip адрес сервера
             ipAddress = InetAddress.getByName(ipd)
-            val socket = Socket(ipAddress, port)
+//            val socket = Socket(ipAddress, port)
             // Создаем сокет
 
 
@@ -115,20 +136,16 @@ class MainActivity : AppCompatActivity() {
 
 
 
-fun onClick() = runBlocking{
+fun onClick() {
     msg = etMessage.text.toString()
-
-
-    val job = launch { netShit( codeMsg ) }
-    job.join()
-
+    launch() { netShit( codeMsg ) }
 }
 
     suspend fun netShit(codeX: Byte){
-        val socket = Socket(ipAddress, port)
-        val outputStream = socket.getOutputStream()
-        val dataOutputStream = DataOutputStream(outputStream)
-            try {
+        try {
+            val socket = Socket(ipAddress, port)
+            val outputStream = socket.getOutputStream()
+            val dataOutputStream = DataOutputStream(outputStream)
                 when (codeX) {
                 // В зависимости от кода команды посылаем сообщения
                     codeMsg    // Сообщение
@@ -149,19 +166,19 @@ fun onClick() = runBlocking{
 
     }
 
-    fun ByteArray.toInt() : Int{
-        var num = -1
-        if(this.isNotEmpty()){
-            val wrapped = ByteBuffer.wrap(this) // big-endian by default
-            num = wrapped.getInt() // 1
-        }
+//    fun ByteArray.toInt() : Int{
+//        var num = -1
+//        if(this.isNotEmpty()){
+//            val wrapped = ByteBuffer.wrap(this) // big-endian by default
+//            num = wrapped.getInt() // 1
+//        }
 
 
 //    val dbuf = ByteBuffer.allocate(2)
 //    dbuf.putShort(num)
 //    val bytes = dbuf.array() // { 0, 1 }
-        return num
-    }
+//        return num
+//    }
 
 
 }
